4399ae11299ea31692b1381ff2a942fbb9ad9ebc
GarretCarmouche <garret@GarretLaptop>
2023-03-01 19:02:11 -0600

    Initialize Spring Project

---
+++ Spring/src/main/java/TIPS/TIPS/TipsApplication.java
@@ -0,0 +1,13 @@
+package TIPS.TIPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipsApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/TIPS/TIPS/TipsApplicationTests.java
@@ -0,0 +1,13 @@
+package TIPS.TIPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
