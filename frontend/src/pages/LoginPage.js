85d880bd8d9df7038dbbe8c91d7e8542b4623a36
hailstorm26 <hstouder@ttu.edu>
2023-04-04 22:05:29 -0500

    pages and navigation

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,18 +1,52 @@
 import React from "react";
 import '../App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Entry from './entryPage';
+import HomePage from './homePage';
+import logo from '../TIPSlogo.png';
+import { useHistory } from 'react-router';
 
 const LoginPage = () => {
+    const history = useHistory();
     return (
-        <div className="App">
-            <div className="App-background">
-                <p> you have reached the login page </p>
-                <div>
-                  <Link className="back-button" to="/"> back </Link> 
+        <Router>
+          <Switch>
+                <Route path="/login">
+                <div className="App">
+                    <div className="App-background">
+                        <p> you have reached the login page </p>
+                        <div>
+                            <Link to="/employee-entry"> employee login </Link> 
+                        </div>
+                        <div>
+                            <div className="back-button" onClick={() => {history.goBack();}}> back </div>
+                        </div> 
+                        <div>
+                            <Link className="next-button" to="/home"> next </Link> 
+                        </div>
+                    </div>     
                 </div> 
-            </div>     
-        </div> 
-
+                </Route>
+                <Route exact path="/">
+                    <div className="App">
+                        <div className="App-background">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p></p>
+                        <div>
+                            <Link className="button" to="/login"> log in </Link> 
+                            <Link className="button" to="/signup"> sign up </Link>
+                        </div>     
+                        </div>
+                    </div>
+                </Route>
+                <Route path="/home">
+                    <HomePage />
+                </Route>
+                <Route path="/employee-entry">
+                    <Entry/>
+                </Route>
+            </Switch>
+        </Router>
     );
 };
 
--- frontend/src/pages/signupPage.js
+++ frontend/src/pages/signupPage.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import '../App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import SignupPage2 from './signupPage2';
+import logo from '../TIPSlogo.png';
+import { useHistory } from 'react-router';
 
 const SignupPage = () =>{
+    const history = useHistory();
     return (
-        <div className="App">
-            <div className='App-background'>
-                <p> you have reached the sign up page </p>
-                <div>
-                  <Link className="back-button" to="/"> back </Link> 
+        <Router>
+          <Switch>
+                <Route path="/signup">
+                <div className="App">
+                    <div className="App-background">
+                        <p> you have reached the sign up page </p>
+                        <div>
+                            <div className="back-button" onClick={() => {history.goBack();}}> back </div>
+                        </div> 
+                        <div>
+                            <Link className="next-button" to="/signup2"> next </Link> 
+                        </div>
+                    </div>     
                 </div> 
-            </div>
-        </div>
+                </Route>
+                <Route exact path="/">
+                    <div className="App">
+                        <div className="App-background">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p></p>
+                        <div>
+                            <Link className="button" to="/login"> log in </Link> 
+                            <Link className="button" to="/signup"> sign up </Link>
+                        </div>     
+                        </div>
+                    </div>
+                </Route>
+                <Route path="/signup2">
+                    <SignupPage2 />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
