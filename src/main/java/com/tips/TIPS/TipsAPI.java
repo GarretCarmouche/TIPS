4bc683e2c7459e57f189c748931c14fbc1b811da
GarretCarmouche <garret307@yahoo.com>
2023-04-06 11:18:56 -0500

    Add REST

--- src/main/java/com/tips/TIPS/TipsAPI.java
+++ src/main/java/com/tips/TIPS/TipsAPI.java
@@ -1,17 +1,27 @@
 package com.tips.TIPS;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.sql.*;
 
-@SpringBootApplication
-public class TipsApplication {
+public class TipsAPI {
     static String ip = "104.154.135.177";
     static int port = 3306;
     static String db = "Tips_dev";
     static String user = "test";
-    static  String pass = "1234";
+    static String pass = "1234";
 
+    /**
+     * This method adds a bartender to the database.
+     * 
+     * @param cardID The ID of the card associated with the bartender.
+     * @param customerID The ID of the customer associated with the bartender.
+     * @param bartenderID The ID of the bartender.
+     * @param bartenderName The name of the bartender.
+     * @param bartenderPhone The phone number of the bartender.
+     * @param bartenderEmail The email address of the bartender.
+     * @param bartenderPass The password of the bartender.
+     * @param bartenderPin The PIN of the bartender.
+     * @return true if the insertion was successful, false otherwise.
+     */
     public static boolean barAdd(String cardID, String customerID, String bartenderID, String bartenderName, String bartenderPhone, String bartenderEmail, String bartenderPass, String bartenderPin){
         int tra = 0;
         String url = "jdbc:mysql://" + ip + ":" + port + "/" + db;
@@ -48,19 +58,28 @@ public class TipsApplication {
         }
     }
 
-    public static boolean cusAdd(String cardID, String customerID, String customerName, String customerPhone, String custEmail, String custPass){
+    /**
+     * This method adds a customer to the database.
+     * 
+     * @param cardID The ID of the card associated with the customer.
+     * @param customerName The name of the customer.
+     * @param customerPhone The phone number of the customer.
+     * @param custEmail The email address of the customer.
+     * @param custPass The password of the customer.
+     * @return true if the insertion was successful, false otherwise.
+     */
+    public static boolean customerAdd(String cardID, String customerName, String customerPhone, String custEmail, String custPass){
         int tra = 0;
         String url = "jdbc:mysql://" + ip + ":" + port + "/" + db;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             try(Connection con = DriverManager.getConnection(url, user, pass)){
-                String query = "INSERT INTO customer (cardID, customerID, customerName, customerPhone, custEmail, custPass) VALUE (?, ?, ?, ?, ?, ?)";
+                String query = "INSERT INTO customer (cardID, customerName, customerPhone, custEmail, custPass) VALUE (?, ?, ?, ?, ?)";
                 PreparedStatement st = con.prepareStatement(query);
                 st.setString(1, cardID);
-                st.setString(2, customerID);
-                st.setString(3, customerName);
-                st.setString(4, customerPhone);
-                st.setString(5, custEmail);
+                st.setString(2, customerName);
+                st.setString(3, customerPhone);
+                st.setString(4, custEmail);
                 st.setString(5, custPass);
                 int insRows = st.executeUpdate();
                 if(insRows > 0){
@@ -72,6 +91,7 @@ public class TipsApplication {
             }
         }
         catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFound");
             e.printStackTrace();
         }
         if(tra == 1){
@@ -82,6 +102,13 @@ public class TipsApplication {
         }
     }
 
+    /**
+     * This method adds a keycard to the database to be associated with a customer for that night or until the customer leaves.
+     * 
+     * @param cardID The ID of the card.
+     * @param customerID The ID of the customer associated with the card.
+     * @return true if the insertion was successful, false otherwise.
+     */
     public static boolean keyAdd(String cardID, String customerID){
         int tra = 0;
         String url = "jdbc:mysql://" + ip + ":" + port + "/" + db;
@@ -112,7 +139,16 @@ public class TipsApplication {
         }
     }
 
-    public static boolean ordAdd(String cardID, String customerID, String orderID, String drinkName){
+    /**
+     * This method adds an order to the database.
+     * 
+     * @param cardID The ID of the card associated with the order.
+     * @param customerID The ID of the customer associated with the order.
+     * @param orderID The ID of the order.
+     * @param drinkName The name of the drink ordered.
+     * @return true if the insertion was successful, false otherwise.
+     */
+    public static boolean orderAdd(String cardID, String customerID, String orderID, String drinkName){
         int tra = 0;
         String url = "jdbc:mysql://" + ip + ":" + port + "/" + db;
         try{
@@ -144,7 +180,20 @@ public class TipsApplication {
         }
     }
 
-    public static boolean payAdd(String cardID, String customerID, String bartenderID, String paymentID, String orderID, String total, String paymentDate, String cardInfo){
+    /**
+     * This method adds a payment to the database.
+     * 
+     * @param cardID The ID of the card associated with the payment.
+     * @param customerID The ID of the customer associated with the payment.
+     * @param bartenderID The ID of the bartender associated with the payment.
+     * @param paymentID The ID of the payment.
+     * @param orderID The ID of the order associated with the payment.
+     * @param total The total amount of the payment.
+     * @param paymentDate The date of the payment.
+     * @param cardInfo The information of the card used for the payment.
+     * @return true if the insertion was successful, false otherwise.
+     */
+    public static boolean paymentAdd(String cardID, String customerID, String bartenderID, String paymentID, String orderID, String total, String paymentDate, String cardInfo){
         int tra = 0;
         String url = "jdbc:mysql://" + ip + ":" + port + "/" + db;
         try{
@@ -180,9 +229,5 @@ public class TipsApplication {
         else{
             return false;
         }
-    }
-    public static void main(String[] args){
-        System.out.println("main");
-        SpringApplication.run(TipsApplication.class, args);
     }
 }
