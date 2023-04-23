0b2fa55155c9094ea22fd2dfbf824bf44fe5058e
xanswine <100490925+xanswine@users.noreply.github.com>
2023-04-23 13:13:36 -0500

    test4

--- src/main/java/com/tips/TIPS/TipsController.java
+++ src/main/java/com/tips/TIPS/TipsController.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("/TIPSController")
-@CrossOrigin(origins = "http://api.tips1.org")
+@CrossOrigin(origins = "http://api.tips1.org:8080")
 
 public class TipsController {
 
