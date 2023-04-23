a3edde6c6ca73be8121888971a205be402d16761
xanswine <100490925+xanswine@users.noreply.github.com>
2023-04-23 13:49:41 -0500

    test5

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,5 @@ COPY pom.xml .
 COPY src ./src
 RUN apt-get update && apt-get install -y maven
 RUN mvn package
-EXPOSE 8080
+EXPOSE 9090
 CMD ["java", "-Dserver.port=${PORT}", "-jar", "target/application.jar"]
-
--- src/main/java/com/tips/TIPS/TipsController.java
+++ src/main/java/com/tips/TIPS/TipsController.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("/TIPSController")
-@CrossOrigin(origins = "http://api.tips1.org")
+@CrossOrigin(origins = "http://api.tips1.org:9090")
 
 public class TipsController {
 
